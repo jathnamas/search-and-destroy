b4c55daecfd8134e668fd49bc98341bbc1cfb524
Samantha Naomi <samanthajardanowski@Samanthas-MBP.lan>
2020-10-28 17:00:18 -0500

    still jumpin

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,47 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
 
-};
+const maxNum = numArr => {
+  
+  
+    for (let i = 0; numArr.length; i ++ ){
+    let maxValue = numArr[0]
+    let numToCheck = numArr[i]
+    if (maxValue > numToCheck){
+      maxValue = numToCheck
+    }
+    
+    return maxValue
+  
+   }
+  }
+  
+  
+  const minJumps = arr => {
+    let length = arr.length
+     //amount of spaces we can jump (min/max)
+    let fakeArr = []
+    let counter = 0
+    
+    6/4 
+  if (arr.length > 0){
+   for (let i = 0, numJumps = arr[i]; i < numJumps ; i ++){
+    fakeArr.push(arr[i])
+    maxNum(fakeArr)
+    counter ++ 
+  
+  
+  } 
+  
+} 
+else {
+    return counter
+}
+   
+  
+  }
+
+
 
 module.exports = minJumps
